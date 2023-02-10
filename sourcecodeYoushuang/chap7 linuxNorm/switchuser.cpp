957b1dcdb7d3af57c79e66ce97e52b5c274c7fc7
liweijie_ubuntu <helloworld.liweili@gmail.com>
2023-02-10 22:22:39 +0800

    :construction: working on chap6: linux server norm

---
+++ sourcecodeYoushuang/chap7 linuxNorm/switchuser.cpp
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <unistd.h>
+
+static bool switch_to_user( uid_t user_id, gid_t gp_id )
+{
+    if ((user_id == 0) && (gp_id == 0)) return false;
+
+    gid_t gid = getgid();
+    uid_t uid = getuid();
+    // not target user or root
+    if (((gid != 0) || (uid != 0)) && ((gid != gp_id) || (uid != user_id))) return false;
+
+    if (uid != 0) return true;
+
+    if ((setgid(gp_id) < 0) || (setuid(user_id) < 0)) return false;
+
+    return true;
+}
