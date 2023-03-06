a878d0457ed512097c09aac2359ea4769c87e4e4
liweijie_ubuntu <helloworld.liweili@gmail.com>
2023-03-06 16:05:23 +0800

    update webserver sourcecode

---
+++ webserver/code/pool/threadpool.h
@@ -0,0 +1,69 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <thread>
+#include <functional>
+#include <assert.h>
+
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t threadCount = 8): pool_(std::make_shared<Pool>()) {
+            assert(threadCount > 0);
+
+            // 创建 threadCount 个线程
+            for(size_t i = 0; i < threadCount; i++) {
+                std::thread([pool = pool_] {
+                    std::unique_lock<std::mutex> locker(pool->mtx);
+                    while(true) {
+                        if(!pool->tasks.empty()) {
+                            auto task = std::move(pool->tasks.front());
+                            pool->tasks.pop();
+                            locker.unlock();
+                            task();
+                            locker.lock();
+                        } 
+                        else if(pool->isClosed) break;
+                        else pool->cond.wait(locker);
+                    }
+                }).detach(); // 线程分离
+            }
+    }
+
+    ThreadPool() = default;
+
+    ThreadPool(ThreadPool&&) = default;
+    
+    ~ThreadPool() {
+        if(static_cast<bool>(pool_)) {
+            {
+                std::lock_guard<std::mutex> locker(pool_->mtx);
+                pool_->isClosed = true;
+            }
+            pool_->cond.notify_all();
+        }
+    }
+
+    template<class F>
+    void AddTask(F&& task) {
+        {
+            std::lock_guard<std::mutex> locker(pool_->mtx);
+            pool_->tasks.emplace(std::forward<F>(task));
+        }
+        pool_->cond.notify_one(); // 随机唤醒
+    }
+
+private:
+    struct Pool {                     // 池子结构体
+        std::mutex mtx;               // 互斥锁
+        std::condition_variable cond; // 条件变量
+        bool isClosed;                // 是否关闭
+        std::queue<std::function<void()>> tasks; // 队列（保存任务）
+    };
+    std::shared_ptr<Pool> pool_;
+};
+
+
+#endif //THREADPOOL_H
---
+++ webserver/readme.md
@@ -0,0 +1,58 @@
+# WebServer
+用C++实现的高性能WEB服务器，经过webbenchh压力测试可以实现上万的QPS
+
+## 功能
+* 利用IO复用技术Epoll与线程池实现多线程的Reactor高并发模型；
+* 利用正则与状态机解析HTTP请求报文，实现处理静态资源的请求；
+* 利用标准库容器封装char，实现自动增长的缓冲区；
+* 基于小根堆实现的定时器，关闭超时的非活动连接；
+* 利用单例模式与阻塞队列实现异步的日志系统，记录服务器运行状态；
+* 利用RAII机制实现了数据库连接池，减少数据库连接建立与关闭的开销，同时实现了用户注册登录功能。
+
+* 增加logsys,threadpool测试单元(todo: timer, sqlconnpool, httprequest, httpresponse) 
+
+
+## 项目启动
+需要先配置好对应的数据库
+```bash
+// 建立yourdb库
+create database yourdb;
+
+// 创建user表
+USE yourdb;
+CREATE TABLE user(
+    username char(50) NULL,
+    password char(50) NULL
+)ENGINE=InnoDB;
+
+// 添加数据
+INSERT INTO user(username, password) VALUES('name', 'password');
+```
+
+```bash
+make
+./bin/server
+```
+
+## 单元测试
+```bash
+cd test
+make
+./test
+```
+
+## 压力测试
+```bash
+./webbench-1.5/webbench -c 100 -t 10 http://ip:port/
+./webbench-1.5/webbench -c 1000 -t 10 http://ip:port/
+./webbench-1.5/webbench -c 5000 -t 10 http://ip:port/
+./webbench-1.5/webbench -c 10000 -t 10 http://ip:port/
+```
+* 测试环境: Ubuntu:19.10 cpu:i5-8400 内存:8G 
+* QPS 10000+
+
+## TODO
+* config配置
+* 完善单元测试
+* 实现循环缓冲区
+
